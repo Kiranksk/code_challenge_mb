ea4f9421b457cf597ad9b2fd20e873680ebef828
Kiranksk <kiran.kurabar@gmail.com>
2021-08-19 10:38:19 +1000

    Update string_count.sh

--- string_count.sh
+++ string_count.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-set -x
 #This script will recursively count all uniq string occurance in file
 
 findoccurrence(){
+
 directory=$(pwd)
 file_extension="*"  # allowed values *, *.pdf, *.sql etc...
+echo $1 $2
 
-if [ ! -z $1 ]; then
-        directory=$1
-else
-        exit 0
-fi
-
-
-if [ ! -z $2 ]; then
-        file_extension="*.$2"
-else
-        exit 0
+#checking the parameters are NOT NULL
+if { [ -d "${1}" ] &&  [ -n "${2}" ]; }; then
+            directory=$1
+            file_extension="*.$2"
 fi
 
 for filename in $(find $directory -type f -name "$file_extension") ; do
@@ -30,26 +24,34 @@ cat total.txt | sort | uniq -c >> count.txt
 echo "Output"
 awk '{print $2,$1}' count.txt
 
+#removing the counter files
 rm -rf count.txt total.txt
 
 }
 
 
 usage() {
-
 echo "
-usage: $script_name -p|--path <folder path>
-                -e|--ext <file extension>
-                [--help]
+usage: sh script_name <folder path> <file extension> | <-h>
 
 Options:
--p|--path <folder path>
--e|--ext <file extension>
---help: show this help
+<folder path> : valid folder path if it is not valid it will take default as PWD
+<file extension> : desired file extension if empty it will take wild card * as default
+-h: show this help
+
+
+eg: sh challenge.sh /home/todo txt   #<To find the .txt file in directory /home/todo>
 "
 }
 
 
 all_params="$@"
-echo $all_params
+#echo $all_params
+if [ "$1" == "-h" ]; then
+        usage
+        exit 0
+fi
+
+#call the main function with all parameters 
 findoccurrence $@
+
